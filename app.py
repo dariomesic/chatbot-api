d41388ba6addd4669c4f17f1c7ca2451dc90e83a
dmesic <dario.mesic@fer.hr>
2023-09-06 14:59:04 +0200

    added api for question manipulation

--- app.py
+++ app.py
@@ -22,21 +22,40 @@ def getIntents():
             query = "SELECT * FROM intents"
             cursor.execute(query)
             items = cursor.fetchall()
-            json_array = [{'id': item[0], 'name': item[1]} for item in items]   #for now this is okay however in future this should have another get for each intent too see how many steps and questions and edit time are there
-
-            # Convert the list of dictionaries to a JSON string
-            intents = json.dumps(json_array, ensure_ascii=False)
-
-            print(intents)
+            json_array = [{'id': item[0], 'name': item[1], 'last_edited': item[2], 'examples_count': item[3], 'steps_count': item[4]} for item in items]
+            
+            # Convert the list of dictionaries to a objects
+            intents = json.dumps(json_array, indent=4, sort_keys=True, default=str) #fixing datetime bug on json parse
 
             cursor.close()
 
-            return jsonify({"intents": intents})
+            return intents
 
         except Exception as e:
-            return jsonify({"error": str(e)})
+            return jsonify(str(e))
         
-@app.route('/getSteps', methods=['GET'])#, 'POST'])
+@app.route('/getQuestionsForIntent', methods=['GET'])
+def getQuestionsForIntent():
+    if request.method == 'GET':
+        try:
+            intent_id = request.args.get('intent_id')
+            cursor = db_connection.cursor()
+            query = f"SELECT * FROM questions WHERE intent_id = {intent_id}"
+            cursor.execute(query)
+            items = cursor.fetchall()
+            json_array = [{'question_id': item[0], 'question': item[1]} for item in items]
+            
+            # Convert the list of dictionaries to a objects
+            questions = json.dumps(json_array, ensure_ascii=False)
+
+            cursor.close()
+
+            return questions
+
+        except Exception as e:
+            return jsonify(str(e))
+        
+@app.route('/getSteps', methods=['GET'])
 def getSteps():
     if request.method == 'GET':
         try:
@@ -53,37 +72,85 @@ def getSteps():
 
             cursor.close()
 
-            return jsonify({"steps": steps})
+            return jsonify(steps)
 
         except Exception as e:
-            return jsonify({"error": str(e)})
+            return jsonify(str(e))
         
 
 @app.route('/postQuestion', methods=['POST'])
 def postQuestion():
     if request.method == 'POST':
         try:
-             data = request.json
-             question = data.get("question")
-             intent_id = data.get("intent_id")
+            data = request.json
+            question = data.get("question")
+            intent_id = data.get("intent_id")
 
-             cursor = db_connection.cursor()
-             query = f"INSERT INTO questions (question_id, question, intent_id) VALUES (DEFAULT, {question}, {intent_id});"
-             cursor.execute(query)
-             db_connection.commit()
-             cursor.close()
+            cursor = db_connection.cursor()
+            query = f"INSERT INTO questions (question_id, question, intent_id) VALUES (DEFAULT, '{question}', {intent_id}) RETURNING question_id;;"
+            cursor.execute(query)
+            db_connection.commit()
 
-             return jsonify({"message": "Question added successfully!"})
+            # Fetch the inserted question_id
+            question_id = cursor.fetchone()[0]
+        
+            cursor.close()
+
+            response = {
+                "message": "Question added successfully!",
+                "question_id": question_id
+            }
+
+            return jsonify(response)
 
         except Exception as e:
-            return jsonify({"error": str(e)})
+            return jsonify(str(e))
 
+@app.route('/deleteQuestion', methods=['DELETE'])
+def deleteQuestion():
+    if request.method == 'DELETE':
+        try:
+
+            question_id = request.args.get('question_id')
+            cursor = db_connection.cursor()
+            query = f"DELETE FROM questions WHERE question_id = '{question_id}';"
+            cursor.execute(query)
+            db_connection.commit()
+            cursor.close()
+
+            return jsonify("Question deleted successfully!")
+
+        except Exception as e:
+            return jsonify(str(e))
+        
+@app.route('/updateQuestion', methods=['PUT'])
+def updateQuestion():
+    if request.method == 'PUT':
+        try:
+            data = request.json
+            question_id = data.get("question_id")
+            new_question = data.get("new_question")
+
+            cursor = db_connection.cursor()
+            query = f"UPDATE questions SET question = '{new_question}' WHERE question_id = {question_id};"
+            cursor.execute(query)
+            db_connection.commit()
+            cursor.close()
+
+            return jsonify("Question updated successfully!")
+
+        except Exception as e:
+            return jsonify(str(e))
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080)
 
 #first api /getAllIntents
 #second api /getAllQuestion/question and #third api /getRules from intent from second api togehter
 #fourth api /addQuestion with intent
-#fifth api /updateRule 
+            #/updateQuestion with intent
+            #/deleteQuestion with intent
+#fifth api /updateRule
+#sixth api /sendQuestion from chatbot for intent
+#seventh api /getResponse when user selects bots answer(this can be done with frontend)
